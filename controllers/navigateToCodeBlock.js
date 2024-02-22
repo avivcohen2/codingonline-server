9a5b7af07634443d84698929bfbfd87fdf161f84
avivcohen2 <78810473+avivcohen2@users.noreply.github.com>
2024-02-22 16:06:56 +0200

    Update navigateToCodeBlock.js

--- controllers/navigateToCodeBlock.js
+++ controllers/navigateToCodeBlock.js
@@ -1,15 +1,15 @@
 const handleNavigationToCodeBlock = (req, res, db) => {
     const { id } = req.params;
-    db.select('*').from('codeblocks').where({id})
-    .then(codeblock => {
-        if (codeblock.length) {
-            console.log('codeblock:', codeblock[0])
-            res.json(codeblock[0]);
-        } else {
-            res.status(400).json('Not found')
-        }
-    })
-    .catch(err => res.status(400).json('error getting the codeblock'))
+    let found = false;
+    db.array.forEach(codeblock => {
+        if (codeblock.id === id) {
+            found = true;
+            res.json(codeblock);
+        } 
+    });
+    if (!found) {
+        res.status(400).json('Not found');
+    }
 }
 
 module.exports = handleNavigationToCodeBlock;
